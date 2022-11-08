3e9209e1ec6aa0b8ce9053abc34d4db5df2f6cac
Luca <lucasantarella@hotmail.it>
2022-11-08 17:44:30 +0100

    sparklines incomplete

---
+++ src/pages/dashboard/Sparklines.js
@@ -0,0 +1,166 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useState, useEffect} from 'react';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import Chart from 'react-apexcharts';
+// third-party
+import ReactApexChart from 'react-apexcharts';
+
+class SLChart extends React.Component {
+        constructor(props) {
+          super(props);
+
+          this.state = {
+
+            series: [{
+              data: [292, 383, 489, 483, 4894, 339]
+            }],
+            options: {
+              chart: {
+                type: 'area',
+                height: 160,
+                sparkline: {
+                  enabled: true
+                },
+              },
+              labels: [...Array(6).keys()].map(n => `2018-09-0${n+1}`),
+              stroke: {
+                curve: 'straight'
+              },
+              fill: {
+                opacity: 0.3,
+              },
+              yaxis: {
+                min: 0
+              },
+              colors: ['#DCE6EC'],
+              title: {
+                text: '$424,652',
+                offsetX: 0,
+                style: {
+                  fontSize: '24px',
+                }
+              },
+              subtitle: {
+                text: 'Sales',
+                offsetX: 0,
+                style: {
+                  fontSize: '14px',
+                }
+              }
+            },
+
+            seriesSpark2: [{
+              data: [292, 383, 489, 483, 4894, 339]
+            }],
+            optionsSpark2: {
+              chart: {
+                type: 'area',
+                height: 160,
+                sparkline: {
+                  enabled: true
+                },
+              },
+              labels: [...Array(6).keys()].map(n => `2018-09-0${n+1}`),
+              stroke: {
+                curve: 'straight'
+              },
+              fill: {
+                opacity: 0.3,
+              },
+              yaxis: {
+                min: 0
+              },
+              colors: ['#DCE6EC'],
+              title: {
+                text: '$235,312',
+                offsetX: 0,
+                style: {
+                  fontSize: '24px',
+                }
+              },
+              subtitle: {
+                text: 'Expenses',
+                offsetX: 0,
+                style: {
+                  fontSize: '14px',
+                }
+              }
+            },
+
+            seriesSpark3: [{
+              data: [292, 383, 489, 483, 4894, 339]
+            }],
+            optionsSpark3: {
+              chart: {
+                type: 'area',
+                height: 160,
+                sparkline: {
+                  enabled: true
+                },
+              },
+              labels: [...Array(6).keys()].map(n => `2018-09-0${n+1}`),
+              stroke: {
+                curve: 'straight'
+              },
+              fill: {
+                opacity: 0.3
+              },
+              xaxis: {
+                crosshairs: {
+                  width: 1
+                },
+              },
+              yaxis: {
+                min: 0
+              },
+              title: {
+                text: '$135,965',
+                offsetX: 0,
+                style: {
+                  fontSize: '24px',
+                }
+              },
+              subtitle: {
+                text: 'Profits',
+                offsetX: 0,
+                style: {
+                  fontSize: '14px',
+                }
+              }
+            },
+          };
+        }
+
+
+
+        render() {
+          return (
+
+      <div className="container">
+  <div className="row">
+    <div className="col-sm">
+      <div id="chart-spark1">
+  <ReactApexChart options={this.state.options} series={this.state.series} type="area" height={160} />
+</div>
+    </div>
+    <div className="col-sm">
+      <div id="chart-spark2">
+  <ReactApexChart options={this.state.optionsSpark2} series={this.state.seriesSpark2} type="area" height={160} />
+</div>
+    </div>
+    <div className="col-sm">
+      <div id="chart-spark3">
+  <ReactApexChart options={this.state.optionsSpark3} series={this.state.seriesSpark3} type="area" height={160} />
+</div>
+    </div>
+  </div>
+</div>
+
+
+          );
+        }
+      }
+export default SLChart;
